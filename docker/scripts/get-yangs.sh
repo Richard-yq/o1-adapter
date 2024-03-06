d2ff4b8ea85e51d37c4afa8f314f796bcd532b15
Adrian Lita <adrian.lita@highstreet-technologies.com>
2024-03-06 11:43:38 +0100

    updated scripts

--- docker/scripts/get-yangs.sh
+++ docker/scripts/get-yangs.sh
@@ -37,7 +37,11 @@ fi
 
 # download
 if [ ! -f "$FILE_DOWNLOADZIP" ]; then
+    # latest commit, working since 2024-01-17
     wget --no-check-certificate -O "$FILE_DOWNLOADZIP" "https://forge.3gpp.org/rep/sa5/MnS/-/archive/Rel-18/MnS-Rel-18.zip?path=yang-models"
+
+    # latest release
+    # wget --no-check-certificate -O "$FILE_DOWNLOADZIP" "https://forge.3gpp.org/rep/sa5/MnS/-/archive/Tag_Rel17_SA96/MnS-Tag_Rel17_SA96.zip?path=yang-models"
 fi
 if [ ! -d "$DIR_AVAILABLE_YANGS" ]; then
     mkdir "$DIR_AVAILABLE_YANGS"
@@ -57,6 +61,7 @@ cp "$DIR_AVAILABLE_YANGS/"ietf-*.yang "$DIR_YANGS/"
 declare special_files=(
     "_3gpp-5g-common-yang-types.yang"
     "_3gpp-5gc-nrm-configurable5qiset.yang"
+    "_3gpp-5gc-ecmconnectioninfo.yang"
     "_3gpp-nr-nrm-bwp.yang"
     "_3gpp-nr-nrm-ep.yang"
     "_3gpp-nr-nrm-gnbcucpfunction.yang"
